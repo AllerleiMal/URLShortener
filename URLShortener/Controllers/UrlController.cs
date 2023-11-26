774097a59fc8a94ef97b06f4dc90f2f81a8ab09f
AllerleiMal <emelpavel2002@gmail.com>
2023-11-26 20:31:26 +0300

    feat: create controllers' skeleton and views' files; change _Layout.cshtml according to new actions' names

---
+++ URLShortener/Controllers/UrlController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace URLShortener.Controllers;
+
+public class UrlController : Controller
+{
+    public UrlController()
+    {
+    }
+
+    [ActionName("Index")]
+    [HttpGet]
+    public async Task<IActionResult> GetUrlMappingList()
+    {
+        return View("Index");
+    }
+
+    [ActionName("AddUrl")]
+    [HttpGet]
+    public async Task<IActionResult> AddUrl()
+    {
+        return View("AddUrl");
+    }
+    
+}
