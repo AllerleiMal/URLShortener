91c7877088351b4636356ff841740c689da724b2
AllerleiMal <emelpavel2002@gmail.com>
2023-11-26 20:13:01 +0300

    feat: add UrlMapping model

---
+++ URLShortener/Models/UrlMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace URLShortener.Models;
+
+public class UrlMapping
+{
+    public long Id { get; set; }
+    public string? LongUrl { get; set; }
+    public string? ShortUrl { get; set; }
+    public long ClickCounter { get; set; }
+}
