4370e7b1c1c03b109f787856e11c6b44a16121d1
AllerleiMal <emelpavel2002@gmail.com>
2023-11-27 11:39:33 +0300

    feat: implement redirection

--- URLShortener/Controllers/RedirectController.cs
+++ URLShortener/Controllers/RedirectController.cs
@@ -1,8 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NHibernate.Linq;
+using URLShortener.Models;
+using ISession = NHibernate.ISession;
 
 namespace URLShortener.Controllers;
 
-public class RedirectController: ControllerBase
+[Route("")]
+public class RedirectController(ISession session) : Controller
 {
-    
+    [HttpGet("{shortUrlCode}")]
+    public async Task<IActionResult> RedirectToLongUrl(string shortUrlCode)
+    {
+        var allMappings = await session.Query<UrlMapping>().ToListAsync();
+        var targetUrlMapping = await session
+            .Query<UrlMapping>()
+            .FirstOrDefaultAsync(u => u.ShortUrlCode.Equals(shortUrlCode));
+
+        if (targetUrlMapping is null)
+        {
+            return NotFound();
+        }
+
+        targetUrlMapping.ClickCounter++;
+        await session.FlushAsync();
+
+        return Redirect(targetUrlMapping.LongUrl!);
+    }
 }
