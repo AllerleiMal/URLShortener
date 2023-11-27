ea288b7751074b493f116d9927ce7c7feed32c21
AllerleiMal <emelpavel2002@gmail.com>
2023-11-27 11:39:19 +0300

    feat: change shortUrl to shortUrlCode

--- URLShortener/Models/UrlMapping.cs
+++ URLShortener/Models/UrlMapping.cs
@@ -4,6 +4,6 @@ public class UrlMapping
 {
     public virtual long Id { get; set; }
     public virtual string? LongUrl { get; set; }
-    public virtual string? ShortUrl { get; set; }
+    public virtual string? ShortUrlCode { get; set; }
     public virtual long ClickCounter { get; set; }
 }
