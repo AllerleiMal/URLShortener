8cdeca0e6302678770809eff8594f25dc5dde297
AllerleiMal <emelpavel2002@gmail.com>
2023-11-28 13:45:52 +0300

    feat: add UrlShortener service

---
+++ URLShortener/Services/IUrlShortener.cs
@@ -0,0 +1,8 @@
+﻿namespace URLShortener.Services;
+
+public interface IUrlShortener
+{
+    bool IsUrlValid(string url);
+    string GenerateShortUrlCode();
+    string CombineShortUrl(string schema, string host, string shortUrlCode);
+}
