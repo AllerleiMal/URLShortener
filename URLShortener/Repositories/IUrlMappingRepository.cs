ca6d6e1d8b43953b15af250f1273e5f599482c6e
AllerleiMal <emelpavel2002@gmail.com>
2023-11-28 14:05:49 +0300

    feat: change void methods to Task methods

--- URLShortener/Repositories/IUrlMappingRepository.cs
+++ URLShortener/Repositories/IUrlMappingRepository.cs
@@ -6,8 +6,8 @@ public interface IUrlMappingRepository
 {
     Task<UrlMapping?> GetUrlMappingAsync(string shortUrlCode);
     Task<UrlMapping?> GetUrlMappingAsync(int id);
-    void AddUrlMappingAsync(UrlMapping? mapping);
+    Task AddUrlMappingAsync(UrlMapping? mapping);
     Task<bool> DeleteUrlMappingAsync(int id);
     IQueryable<UrlMapping> GetUrlMappingsQuery();
-    void SaveChangesAsync();
+    Task SaveChangesAsync();
 }
