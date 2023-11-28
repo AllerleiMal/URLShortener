b3126954bfae5fe306776ceca13fa90f020fb89e
AllerleiMal <emelpavel2002@gmail.com>
2023-11-28 13:37:12 +0300

    feat: add viewmodel for mapping creation and edition

---
+++ URLShortener/Models/ManageShortUrlViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortener.Models;
+
+public class ManageShortUrlViewModel
+{
+    [Url]
+    public string? LongUrl { get; set; }
+    [Url]
+    public string? ShortUrl { get; set; }
+}
