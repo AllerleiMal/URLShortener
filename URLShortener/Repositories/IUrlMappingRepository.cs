a6a5092a9b70abd2829c2f58e0c2fc42ee43372b
AllerleiMal <emelpavel2002@gmail.com>
2023-11-28 14:01:58 +0300

    feat: add url mapping repository

---
+++ URLShortener/Repositories/IUrlMappingRepository.cs
@@ -0,0 +1,12 @@
+﻿using URLShortener.Models;
+
+namespace URLShortener.Repositories;
+
+public interface IUrlMappingRepository
+{
+    Task<UrlMapping?> GetUrlMappingAsync(string shortUrlCode);
+    Task<UrlMapping?> GetUrlMappingAsync(int id);
+    void AddUrlMappingAsync(UrlMapping? mapping);
+    Task<bool> DeleteUrlMappingAsync(int id);
+    IQueryable<UrlMapping> GetUrlMappingsQuery();
+}
